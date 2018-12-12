0a68b5280590f7c309433a5e19a1bc7bd592d424
bach2Prime <o1907693@nwytg.net>
2018-12-12 12:31:57 +0600

    Regular Session set as default for foreign currency

---
+++ Service/Pbm Ack  Updater/Uninstall.bat
@@ -0,0 +1,32 @@
+@echo off
+CLS
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift & goto gotPrivileges)
+etlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
+ECHO args = "ELEV " >> "%temp%\OEgetPrivileges.vbs"
+ECHO For Each strArg in WScript.Arguments >> "%temp%\OEgetPrivileges.vbs"
+ECHO args = args ^& strArg ^& " "  >> "%temp%\OEgetPrivileges.vbs"
+ECHO Next >> "%temp%\OEgetPrivileges.vbs"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+"%SystemRoot%\System32\WScript.exe" "%temp%\OEgetPrivileges.vbs" %*
+exit /B
+
+:gotPrivileges
+if '%1'=='ELEV' shift
+setlocal & pushd .
+cd /d %~dp0
+
+cls
+
+tasklist /FI "IMAGENAME eq mmc.exe" 2>NUL | find /I /N "mmc.exe">NUL
+if "%ERRORLEVEL%"=="0" taskkill /f /t /im mmc.exe
+net stop "Pbm Ack Parser"
+sc delete "Pbm Ack Parser"
+pause
---
+++ Service/Pbm File Downloader/Install.bat
@@ -0,0 +1,35 @@
+echo off
+CLS
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift & goto gotPrivileges)
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
+ECHO args = "ELEV " >> "%temp%\OEgetPrivileges.vbs"
+ECHO For Each strArg in WScript.Arguments >> "%temp%\OEgetPrivileges.vbs"
+ECHO args = args ^& strArg ^& " "  >> "%temp%\OEgetPrivileges.vbs"
+ECHO Next >> "%temp%\OEgetPrivileges.vbs"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+"%SystemRoot%\System32\WScript.exe" "%temp%\OEgetPrivileges.vbs" %*
+exit /B
+
+:gotPrivileges
+cd /d %~dp0
+if '%1'=='ELEV' shift
+setlocal & pushd .
+
+@SET INSTALLUTILDIR=
+@for /F "tokens=1,2*" %%i in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" /v "InstallPath"') DO (
+    if "%%i"=="InstallPath" (
+        SET "INSTALLUTILDIR=%%k"
+    )
+)
+
+%INSTALLUTILDIR%\installutil CommlinkBachFtpClientService.exe
+pause
